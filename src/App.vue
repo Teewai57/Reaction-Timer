d189a6ba4ba9cfe60ce4a9919a27d15701c97510
Teewai57 <bobtoyoabasi@gmail.com>
2022-05-25 12:42:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p>Reaction time: {{score}} ms</p>
+</template>
+
+<script>
+import Block from "./components/block.vue"
+export default {
+  name: 'App',
+  components: { Block },
+  data () {
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+}
+</style>
